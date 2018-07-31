b23d01b5c5aa4e23cdf4cc13966c1f75f10a89c4
Amalie S <amaliesmidth@gmail.com>
2018-07-31 14:34:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,10 @@
-def display_board
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end 
+
